ee0c6eeab5bc67ef269408c9a12a7f246741fb0a
pejoedev <jondevpet@gmail.com>
2026-03-19 19:16:24 +0100

    update leftPad to work with all strings

--- src/helpers/leftPad.js
+++ src/helpers/leftPad.js
@@ -1,13 +1,19 @@
-// TODO: method comments
-function leftPad(number, minCharacters, placeCharacter = " ") {
+/**
+ * Pads a string with characters on the left to reach a minimum length
+ * @param {string|number} string - The string (or number) to pad
+ * @param {number} minCharacters - The minimum length of the resulting string
+ * @param {string} placeCharacter - The character to use for padding (default: space)
+ * @returns {string} The padded string
+ */
+function leftPad(string, minCharacters, placeCharacter = " ") {
     let returnString = "";
-    let leng = `${number}`.length;
+    let leng = `${string}`.length;
     if (leng < minCharacters) {
         for (let i = 0; i < minCharacters - leng; i++) {
             returnString += placeCharacter;
         }
     }
-    returnString += `${number}`;
+    returnString += `${string}`;
     return returnString;
 }
 
