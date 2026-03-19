6d3c7e3f60b63614189b4f4128d27c9a4dc05f6a
pejoedev <jondevpet@gmail.com>
2026-03-19 18:09:02 +0100

    creating my own leftPad

---
+++ src/helpers/leftPad.js
@@ -0,0 +1,13 @@
+function leftPad(number, minCharacters, placeCharacter = " ") {
+    let returnString = "";
+    let leng = `${number}`.length;
+    if (leng < minCharacters) {
+        for (let i = 0; i < minCharacters - leng; i++) {
+            returnString += placeCharacter;
+        }
+    }
+    returnString += `${number}`;
+    return returnString;
+}
+
+module.exports = { leftPad };
